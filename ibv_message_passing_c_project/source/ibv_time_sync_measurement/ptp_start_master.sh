1ed799ccf06fa841f39048b36c5c0b91e4cd325d
Chester Gillon <chester.gillon@metronet.co.uk>
2018-09-30 17:27:34 +0100

    Add the -O 0 option to phc2sys to use a zero TAI to UTC offset, which avoids the CLOCK_REALTIME on the master going backwards by 36 seconds when PTP is started.

--- ibv_message_passing_c_project/source/ibv_time_sync_measurement/ptp_start_master.sh
+++ ibv_message_passing_c_project/source/ibv_time_sync_measurement/ptp_start_master.sh
@@ -2,4 +2,4 @@
 # Start PTP as master
 
 sudo -b nohup ptp4l -i eno1 > /dev/null
-sudo -b nohup sudo phc2sys -s /dev/ptp0 -r -w > /dev/null
+sudo -b nohup sudo phc2sys -s /dev/ptp0 -r -w -O 0 > /dev/null
--- ibv_message_passing_c_project/source/ibv_time_sync_measurement/ptp_start_slave.sh
+++ ibv_message_passing_c_project/source/ibv_time_sync_measurement/ptp_start_slave.sh
@@ -2,4 +2,4 @@
 # Start PTP as slave
 
 sudo -b nohup ptp4l -i eno1 -s > /dev/null
-sudo -b nohup sudo phc2sys -s /dev/ptp0 -r -w > /dev/null
+sudo -b nohup sudo phc2sys -s /dev/ptp0 -r -w -O 0 > /dev/null
