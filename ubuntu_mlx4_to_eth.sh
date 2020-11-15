2aeba3d2d6230c59ab0ced097af1f6478f5ed457
Chester Gillon <chester.gillon@metronet.co.uk>
2020-11-15 14:21:31 +0000

    Add ubuntu_mlx4_to_eth.sh. Change centos_mlx4_to_eth.sh so set the IP addresses on the expected ports; removing file added incorrectly in previous commit

--- ubuntu_mlx4_to_eth.sh
+++ ubuntu_mlx4_to_eth.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 # Quick hack for a ConnectX-2 VPI to switch the ports from Infiniband to Ethernet.
-# @todo assumes single dual-port card and Ethernet interface names for a CentOS 6 installation.
-
-# Need to stop the ibacm server as otherwise unable to change the link type
-service ibacm stop
+# @todo assumes single dual-port card and Ethernet interface names for a Ubuntu installation.
 
 # Switch the ports to Ethernet
 echo eth > /sys/module/mlx4_core/drivers/pci\:mlx4_core/00*/mlx4_port1
@@ -11,17 +8,11 @@ echo eth > /sys/module/mlx4_core/drivers/pci\:mlx4_core/00*/mlx4_port2
 
 # Ensure the module for Ethernet is loaded. May have not been loaded during boot when the Connect-X2 defaults to Infiniband
 modprobe mlx4_en
-sleep 2
 
 # Enable the Ethernet devices and add IP addresses.
 # RoCE can operate without the IP addresses being set.
 # Different subnets are used for each port, as operating with direct cables rather than a switch.
-# The expression to set the IP address from the dev_id is due to indeterminate behavioral about which eth1/eth2 device is which port
-ifnums="1 2"
-for if in ${ifnums}
-do
-   dev_id=$(< /sys/class/net/eth${if}/dev_id)
-   ifconfig eth${if} up
-   ifconfig eth${if} 192.$((169 + ${dev_id})).0.$((1 + ${dev_id}))
-done
-
+ifconfig ens5 up
+ifconfig ens5d1 up
+ifconfig ens5 192.169.0.3
+ifconfig ens5d1 192.170.0.4
