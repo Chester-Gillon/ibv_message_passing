6042ff51dec448446f5ebd4fbc4069590eed0438
Chester Gillon <chester.gillon@metronet.co.uk>
2021-12-29 18:39:04 +0000

    Rework Infiniband-to-Ethernet script used on the Ubuntu host: a. Be more deterministic, by reloading the the driver modules with Ethernet specified as the link-layer and waiting for the Ethernet devices to be ready. b. Avoiding conflicts with the NetworkManager either attempting to use DHCP or preventing IPv6 link-scope addresses being allocated. c. Remove the setting of IPv4 addresses, which are taking a GID entry, since IPv6 link-scope addresses are sufficient for RDMA when all hosts are on the same switched network.

--- ubuntu_mlx4_to_eth.sh
+++ ubuntu_mlx4_to_eth.sh
@@ -1,18 +1,68 @@
 #! /bin/bash
-# Quick hack for a ConnectX-2 VPI to switch the ports from Infiniband to Ethernet.
+# Script for a ConnectX-2 VPI to switch the ports from Infiniband to Ethernet.
 # @todo assumes single dual-port card and Ethernet interface names for a Ubuntu installation.
 
-# Switch the ports to Ethernet
-echo eth > /sys/module/mlx4_core/drivers/pci\:mlx4_core/00*/mlx4_port1
-echo eth > /sys/module/mlx4_core/drivers/pci\:mlx4_core/00*/mlx4_port2
+if [[ $(id -u) -ne 0 ]]
+then
+   echo "Please run as root"
+   exit 1
+fi
 
-# Ensure the module for Ethernet is loaded. May have not been loaded during boot when the Connect-X2 defaults to Infiniband
-modprobe mlx4_en
+# Tell NetworkManager that the ConnectX-2 devices using the mlx4_core driver are unmanaged.
+# Having the devices as unmanaged ensures no conflicts from NetworkManager perform the following:
+# a. Attempting to use DHCP to allocate an address.
+# b. Preventing IPv6 link-local addresses being allocated.
+#
+# This also means if the ConnectX-2 devices are Infiniband they are also unmanaged by NetworkManager
+#
+# The file created persists across reboots.
+#
+# @todo Doesn't check that are overriding any other [keyfile] sections in other NetworkManager
+#       configuration files.
+nm_dev_ignore_file=/etc/NetworkManager/conf.d/99-unmanaged-devices.conf
+if [ ! -f ${nm_dev_ignore_file} ]
+then
+   cat << EOF >> ${nm_dev_ignore_file}
+[keyfile]
+unmanaged-devices=driver:mlx4_core
+EOF
+   echo "Restarting Network Manager to take note of the unmanaged devices"
+   systemctl restart NetworkManager
+fi
 
-# Enable the Ethernet devices and add IP addresses.
-# RoCE can operate without the IP addresses being set.
-# Different subnets are used for each port, as operating with direct cables rather than a switch.
-ifconfig ens5 up
-ifconfig ens5d1 up
-ifconfig ens5 192.169.0.3
-ifconfig ens5d1 192.170.0.4
+# Remove the existing driver modules
+rmmod mlx4_en mlx4_ib mlx4_core
+
+# Reload the driver module, specifying the port type as Ethernet (2)
+modprobe mlx4_core port_type_array=2,2
+
+# The names of the Ethernet devices for each of the ports on the ConnectX-2 VPI card enumerate as.
+mlx4_ethernet_devices="ens5 ens5d1"
+
+for dev in ${mlx4_ethernet_devices}
+do
+   # Poll for the device to be created, as may be a delay after the driver module is loaded before the device appears
+   echo "Waiting for ${dev} to be created"
+   until [ -d /sys/class/net/${dev} ]
+   do
+      sleep 1
+   done
+
+   # Bring the device up. No specific IP address is set, but will have a IPv6 link-scope address automatically allocated
+   ip link set ${dev} up
+
+   # Set the MTU to the maximum supported by the T1700G-28TQ switch.
+   ip link set ${dev} mtu 9216
+
+   # Wait for IPv6 duplicate address detection to complete
+   echo "Waiting for ${dev} duplicate address detection to complete"
+   while [[ `ip -6 addr show ${dev} | grep -c "scope link"` -eq "0" ]]
+   do
+      sleep 1
+   done
+   until [[ `ip -6 addr show ${dev} | grep -c "tentative"` -eq "0" ]]
+   do
+      sleep 1
+   done
+done
+
