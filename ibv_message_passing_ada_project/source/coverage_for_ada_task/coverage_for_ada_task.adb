f8c0c5014e96fbeb001cb9d3e00e4f695ca9a456
Chester Gillon <chester.gillon@metronet.co.uk>
2022-05-29 18:24:10 +0100

    Add coverage test for simple generic package

--- ibv_message_passing_ada_project/source/coverage_for_ada_task/coverage_for_ada_task.adb
+++ ibv_message_passing_ada_project/source/coverage_for_ada_task/coverage_for_ada_task.adb
@@ -6,6 +6,19 @@
 with Ada.Text_IO;
 
 procedure Coverage_For_Ada_Task is
+   generic
+      Name : String;
+   package Generic_Name is
+      procedure Display_Name;
+   end Generic_Name;
+
+   package body Generic_Name is
+      procedure Display_Name is
+      begin
+         Ada.Text_IO.Put_Line ("My name is " & Name);
+      end Display_Name;
+   end Generic_Name;
+
    task Print_Task is
       entry Print;
    end Print_Task;
@@ -22,10 +35,15 @@ procedure Coverage_For_Ada_Task is
          end select;
       end loop;
    end Print_Task;
+
+   package Package_A is new Generic_Name ("A");
+   package Package_B is new Generic_Name ("B");
 begin
+   Package_A.Display_Name;
    for index in 1..3
    loop
       Print_Task.Print;
    end loop;
+   Package_B.Display_Name;
    Ada.Text_IO.Put_Line ("In main");
 end Coverage_For_Ada_Task;
