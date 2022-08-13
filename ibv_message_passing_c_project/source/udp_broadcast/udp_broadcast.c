9bf67019ceb1bfdc73c5e683bae838426fd4f9eb
Chester Gillon <chester.gillon@metronet.co.uk>
2022-08-13 17:53:05 +0100

    Add display of interface broadcast statistics during test

--- ibv_message_passing_c_project/source/udp_broadcast/udp_broadcast.c
+++ ibv_message_passing_c_project/source/udp_broadcast/udp_broadcast.c
@@ -21,6 +21,9 @@
 #include <poll.h>
 #include <net/if.h>
 #include <ifaddrs.h>
+#include <sys/ioctl.h>
+#include <linux/ethtool.h>
+#include <linux/sockios.h>
 
 #define NSECS_PER_SEC 1000000000LL
 
@@ -88,6 +91,25 @@ static uint32_t num_source_ips;
 static per_source_ip_t source_ips[MAX_SOURCE_IPS];
 
 
+/* Used to obtain the ethtool statistics for one network interface */
+typedef struct interface_statistics_s
+{
+    /* Used to create a linked list */
+    struct interface_statistics_s *next;
+    /* Socket used for ioctl */
+    int fd;
+    /* Interface request structure used for ioctl */
+    struct ifreq ifr;
+    /* The number of statistics for the interface */
+    uint32_t num_statistics;
+    /* Names of the interface statistics */
+    struct ethtool_gstrings *statistic_names;
+    /* The statistic values before and after the test, so can see the number of broadcast packets during the test */
+    struct ethtool_stats *statistics_before;
+    struct ethtool_stats *statistics_after;
+} interface_statistics_t;
+
+
 /* Set from a signal handler to request the test is stopped */
 static volatile bool stop_transmission;
 
@@ -97,6 +119,232 @@ static volatile bool stop_transmission;
 static void stop_transmission_handler (const int sig)
 {
     stop_transmission = true;
+}
+
+
+/**
+ * @brief Get the current values of the statistics for one interface
+ * @param[in/out] if_stats Which interface to get the statistics for
+ * @param[in] before Selects if to save the statistics for before (true) or after (false) the test.
+ */
+static void get_interface_statistics (interface_statistics_t *const if_stats, const bool before)
+{
+    if (if_stats->num_statistics > 0)
+    {
+        struct ethtool_stats *const stats_buf = before ? if_stats->statistics_before : if_stats->statistics_after;
+        int rc;
+
+        stats_buf->cmd = ETHTOOL_GSTATS;
+        stats_buf->n_stats = if_stats->num_statistics;
+        if_stats->ifr.ifr_data = (caddr_t) stats_buf;
+        rc = ioctl (if_stats->fd, SIOCETHTOOL, &if_stats->ifr);
+        if (rc != 0)
+        {
+            printf ("ETHTOOL_GSTATS failed for %s\n", if_stats->ifr.ifr_name);
+            exit (EXIT_FAILURE);
+        }
+    }
+}
+
+
+/**
+ * @brief Initialise the structure used to collect the statistics for one network interface.
+ * @param[in] ifa_name The name of the interface to collect the statistics for
+ * @return The initialised structure
+ */
+static interface_statistics_t *initialise_interface_statistics (const char *const ifa_name)
+{
+    struct
+    {
+        struct ethtool_sset_info hdr;
+        uint32_t buf[1];
+    } sset_info;
+    int rc;
+
+    interface_statistics_t *const if_stats = calloc (1, sizeof (*if_stats));
+
+    /* Open the socket used for ioctl's */
+    if_stats->fd = socket (AF_INET, SOCK_DGRAM, 0);
+    if (if_stats->fd == -1)
+    {
+        printf ("socket() failed\n");
+        exit (EXIT_FAILURE);
+    }
+
+    /* Get the number of statistics supported by the network interface.
+     * May be zero on virtual interfaces such as "lo". */
+    sset_info.hdr.cmd = ETHTOOL_GSSET_INFO;
+    sset_info.hdr.reserved = 0;
+    sset_info.hdr.sset_mask = 1ULL << ETH_SS_STATS;
+
+    snprintf (if_stats->ifr.ifr_name, sizeof (if_stats->ifr.ifr_name), "%s", ifa_name);
+    if_stats->ifr.ifr_data = (caddr_t) &sset_info;
+    rc = ioctl (if_stats->fd, SIOCETHTOOL, &if_stats->ifr);
+    if (rc != 0)
+    {
+        printf ("ETHTOOL_GSSET_INFO failed for %s\n", ifa_name);
+        exit (EXIT_FAILURE);
+    }
+
+    if_stats->num_statistics = (sset_info.hdr.sset_mask != 0) ? sset_info.hdr.data[0] :0;
+
+    if (if_stats->num_statistics > 0)
+    {
+        /* Get the names of the interface statistics */
+        const size_t strings_len = sizeof (*if_stats->statistic_names) + (if_stats->num_statistics * ETH_GSTRING_LEN);
+        if_stats->statistic_names = calloc (1, strings_len);
+        if (if_stats->statistic_names == NULL)
+        {
+            printf ("Failed to allocate %zu bytes for statistics_names\n", strings_len);
+            exit (EXIT_FAILURE);
+        }
+
+        if_stats->statistic_names->cmd = ETHTOOL_GSTRINGS;
+        if_stats->statistic_names->string_set = ETH_SS_STATS;
+        if_stats->statistic_names->len = if_stats->num_statistics;
+        if_stats->ifr.ifr_data = (caddr_t) if_stats->statistic_names;
+        rc = ioctl (if_stats->fd, SIOCETHTOOL, &if_stats->ifr);
+        if (rc != 0)
+        {
+            printf ("ETHTOOL_GSTRINGS failed for %s\n", ifa_name);
+            exit (EXIT_FAILURE);
+        }
+
+        /* Allocate space to store the statistic values */
+        const size_t stats_len = sizeof (struct ethtool_stats) + (if_stats->num_statistics * sizeof (uint64_t));
+
+        if_stats->statistics_before = calloc (1, stats_len);
+        if_stats->statistics_after = calloc (1, stats_len);
+        if ((if_stats->statistics_before == NULL) || (if_stats->statistics_after == NULL))
+        {
+            printf ("Failed to allocate %zu bytes for statistic values\n", stats_len);
+            exit (EXIT_FAILURE);
+        }
+    }
+
+    return if_stats;
+}
+
+
+/**
+ * @brief Get the statistics for all network interfaces before the start of a test
+ * @param[in] interface_addresses The list of all network interfaces on the local host
+ * @return Returns the head of a linked list containing the statistics for all network interfaces
+ */
+static interface_statistics_t *get_interface_statistics_before_test (struct ifaddrs *const interface_addresses)
+{
+    struct ifaddrs *iap = interface_addresses;
+    interface_statistics_t *head = NULL;
+    interface_statistics_t *tail = NULL;
+
+    while (iap != NULL)
+    {
+        /* The struct ifaddrs link-list can have the same interface multiple times, once for each IP address.
+         * Therefore, add each interface to the statistics linked-list only once */
+        bool ifa_name_already_found = false;
+        for (const interface_statistics_t *existing_if = head;
+                (existing_if != NULL) && (!ifa_name_already_found);
+                existing_if = existing_if->next)
+        {
+            if (strcmp (iap->ifa_name, existing_if->ifr.ifr_name) == 0)
+            {
+                ifa_name_already_found = true;
+            }
+        }
+
+        if (!ifa_name_already_found)
+        {
+            interface_statistics_t *const if_stats = initialise_interface_statistics (iap->ifa_name);
+
+            get_interface_statistics (if_stats, true);
+            if (head == NULL)
+            {
+                head = if_stats;
+            }
+            if (tail != NULL)
+            {
+                tail->next = if_stats;
+            }
+            tail = if_stats;
+            if_stats->next = NULL;
+        }
+
+        iap = iap->ifa_next;
+    }
+
+    return head;
+}
+
+
+/**
+ * @brief Get the statistics for all network interfaces after completion of a test
+ * @param[in/out] head The linked list of all interfaces to get the statistics for
+ */
+static void get_interface_statistics_after_test (interface_statistics_t *const head)
+{
+    interface_statistics_t *if_stats = head;
+
+    while (if_stats != NULL)
+    {
+        get_interface_statistics (if_stats, false);
+        if_stats = if_stats->next;
+    }
+}
+
+
+/**
+ * @brief Display the change in broadcast packet statistics for all interfaces on the host during the test
+ * @param[in] head The link-listed of statistics fro all interfaces on the host
+ */
+static void display_broadcast_interface_statistics (const interface_statistics_t *const head)
+{
+    const interface_statistics_t *if_stats = head;
+
+    /* Prefixes of ethtool statistic names which report the numbers of broadcast packets.
+     * Taken from a sample of network interfaces. */
+    static const char *const broadcast_statistic_prefixes[] =
+    {
+        "rx_broadcast",
+        "tx_broadcast"
+    };
+
+    while (if_stats != NULL)
+    {
+        bool interface_header_output = false;
+
+        for (uint32_t stats_index = 0; stats_index < if_stats->num_statistics; stats_index++)
+        {
+            const char *const statistic_name = (const char *) &if_stats->statistic_names->data[stats_index * ETH_GSTRING_LEN];
+
+            for (uint32_t prefix_index = 0;
+                    prefix_index < (sizeof (broadcast_statistic_prefixes) / sizeof (broadcast_statistic_prefixes[0]));
+                    prefix_index++)
+            {
+                const char *const prefix = broadcast_statistic_prefixes[prefix_index];
+
+                if (strncmp (statistic_name, prefix, strlen (prefix)) == 0)
+                {
+                    const uint64_t count_before = if_stats->statistics_before->data[stats_index];
+                    const uint64_t count_after = if_stats->statistics_after->data[stats_index];
+
+                    if (!interface_header_output)
+                    {
+                        printf ("Changes to broadcast statistics for interface %s during test:\n", if_stats->ifr.ifr_name);
+                        interface_header_output = true;
+                    }
+                    printf ("  %s : %" PRIu64 " (%" PRIu64 " to %" PRIu64 ")\n",
+                            statistic_name, count_after - count_before, count_before, count_after);
+                }
+            }
+        }
+
+        if (interface_header_output)
+        {
+            printf ("\n");
+        }
+
+        if_stats = if_stats->next;
+    }
 }
 
 
@@ -132,7 +380,7 @@ int main (int argc, char *argv[])
         exit (EXIT_FAILURE);
     }
 
-    /* Get all interfaces to be able to lookup interface for source IP address */
+    /* Get all interfaces to be able to lookup interface for source IP address, and to get interface statistics */
     struct ifaddrs *interface_addresses = NULL;
     rc = getifaddrs (&interface_addresses);
     if (rc != 0)
@@ -192,6 +440,8 @@ int main (int argc, char *argv[])
 
         num_source_ips++;
     }
+
+    interface_statistics_t *const if_stats_head = get_interface_statistics_before_test (interface_addresses);
 
     /* Initialise the sockets used for each source IP address */
     for (ip_index = 0; ip_index < num_source_ips; ip_index++)
@@ -391,6 +641,8 @@ int main (int argc, char *argv[])
         }
     }
 
+    get_interface_statistics_after_test (if_stats_head);
+
     /* Display the results of the test */
     const int64_t test_end_time = get_monotonic_time ();
     const double test_duration_seconds = (double) (test_end_time - test_start_time) / 1E9;
@@ -415,7 +667,7 @@ int main (int argc, char *argv[])
             inet_ntop_status = inet_ntop (AF_INET, &rx_source->s_addr, ip_addr_string, sizeof (ip_addr_string));
             if (inet_ntop_status == NULL)
             {
-                snprintf (ip_addr_string, sizeof (ip_addr_string), "<inet_ntop() failed>");
+                snprintf (ip_addr_string, sizeof (ip_addr_string), "%s", "<inet_ntop() failed>");
             }
             printf ("  [%" PRIu32 "] source_from_recvfrom=%s  source_from_message=%s\n",
                     rx_source_index, ip_addr_string, rx_source->first_message.source_ip_addr_string);
@@ -426,6 +678,8 @@ int main (int argc, char *argv[])
         }
         printf ("\n");
     }
+
+    display_broadcast_interface_statistics (if_stats_head);
 
     return EXIT_SUCCESS;
 }
