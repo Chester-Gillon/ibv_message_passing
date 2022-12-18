05f2926350105990d85ae7b453a791ace1d4641f
Chester Gillon <chester.gillon@metronet.co.uk>
2022-12-18 00:28:44 +0000

    Make the ports ready, so that the displayed fast-path function pointers are not accurate.

--- ibv_message_passing_c_project/source/dpdk_switch_test/dpdk_information.c
+++ ibv_message_passing_c_project/source/dpdk_switch_test/dpdk_information.c
@@ -7,6 +7,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <inttypes.h>
 
@@ -31,6 +32,19 @@ int main (int argc, char *argv[])
     struct rte_eth_fc_conf fc_conf;
     char if_name[IF_NAMESIZE];
     uint64_t offload;
+    struct rte_eth_conf port_conf;
+    uint16_t nb_rxd;
+    uint16_t nb_txd;
+    struct rte_mempool *mbuf_pool;
+    char mbuf_pool_name[64];
+    struct rte_eth_rxq_info rx_qinfo;
+    struct rte_eth_txq_info tx_qinfo;
+
+    /* Only need a single queue per device to be able to initialise the device to determine
+     * the available fast-path operations. */
+    const uint16_t num_rx_queues = 1;
+    const uint16_t num_tx_queues = 1;
+    const uint16_t queue_id = 0;
 
     /* For testing display of application usage, i.e. other than the description of the EAL options */
     (void) rte_set_application_usage_hook (display_usage);
@@ -146,6 +160,91 @@ int main (int argc, char *argv[])
         }
         printf ("\n");
 
+        /* Configure device using defaults */
+        memset (&port_conf, 0, sizeof (port_conf));
+        rc = rte_eth_dev_configure (port_id, num_rx_queues, num_tx_queues, &port_conf);
+        if (rc != 0)
+        {
+            printf ("rte_eth_dev_configure() failed : %s\n", strerror (-rc));
+        }
+
+        /* See what minimum number of descriptors is converted to */
+        nb_rxd = 1;
+        nb_txd = 1;
+        rc = rte_eth_dev_adjust_nb_rx_tx_desc (port_id, &nb_rxd, &nb_txd);
+        if (rc == 0)
+        {
+            printf ("  adjusted nb_rxd=%" PRIu16 " nb_txd=%" PRIu16 "\n", nb_rxd, nb_txd);
+        }
+        else
+        {
+            printf ("rte_eth_dev_adjust_nb_rx_tx_desc() failed : %s\n", strerror (-rc));
+        }
+
+        /* Create a pool with the number of descriptors, which are allocated when rte_eth_dev_start() is called */
+        snprintf (mbuf_pool_name, sizeof (mbuf_pool_name), "MBUF_POOL_%" PRIu16, port_id);
+        mbuf_pool = rte_pktmbuf_pool_create (mbuf_pool_name,
+                                             nb_rxd,
+                                             0,
+                                             0,
+                                             RTE_MBUF_DEFAULT_BUF_SIZE,
+                                             rte_socket_id ());
+        if (mbuf_pool == NULL)
+        {
+            printf ("rte_pktmbuf_pool_create() failed\n");
+            (void) rte_eal_cleanup();
+            exit (EXIT_FAILURE);
+        }
+
+        /* Allocate and setup 1 RX queue for the port */
+        rc = rte_eth_rx_queue_setup (port_id, queue_id, nb_rxd, rte_eth_dev_socket_id (port_id), NULL, mbuf_pool);
+        if (rc == 0)
+        {
+            rc = rte_eth_rx_queue_info_get (port_id, queue_id, &rx_qinfo);
+            if (rc == 0)
+            {
+                printf ("  rx_qinfo nb_desc=%" PRIu16 " rx_buf_size=%" PRIu16 "\n",
+                        rx_qinfo.nb_desc, rx_qinfo.rx_buf_size);
+            }
+            else
+            {
+                printf ("rte_eth_rx_queue_info_get() failed : %s\n", strerror (-rc));
+            }
+        }
+        else
+        {
+            printf ("rte_eth_rx_queue_setup() failed : %s\n", strerror (-rc));
+        }
+
+        /* Allocate and setup 1 TX queue for the port */
+        rc = rte_eth_tx_queue_setup (port_id, queue_id, nb_txd, rte_eth_dev_socket_id (port_id), NULL);
+        if (rc == 0)
+        {
+            rc = rte_eth_tx_queue_info_get (port_id, queue_id, &tx_qinfo);
+            if (rc == 0)
+            {
+                printf ("  tx_qinfo nb_desc=%" PRIu16 " pthresh=%" PRIu8 " hthresh=%" PRIu8 " wthresh=%" PRIu8 " tx_rs_thresh=%" PRIu16 " tx_free_thresh=%" PRIu16 "\n",
+                        tx_qinfo.nb_desc,
+                        tx_qinfo.conf.tx_thresh.pthresh, tx_qinfo.conf.tx_thresh.hthresh, tx_qinfo.conf.tx_thresh.wthresh,
+                        tx_qinfo.conf.tx_rs_thresh, tx_qinfo.conf.tx_free_thresh);
+            }
+            else
+            {
+                printf ("rte_eth_tx_queue_info_get() failed : %s\n", strerror (-rc));
+            }
+        }
+        else
+        {
+            printf ("rte_eth_tx_queue_setup() failed : %s\n", strerror (-rc));
+        }
+
+        /* Start the Ethernet port */
+        rc = rte_eth_dev_start (port_id);
+        if (rc != 0)
+        {
+            printf ("rte_eth_dev_start() failed : %s\n", strerror (-rc));
+        }
+
         rc = rte_eth_link_get (port_id, &link);
         if (rc == 0)
         {
@@ -231,6 +330,24 @@ int main (int argc, char *argv[])
                 ops->rx_pkt_burst, ops->rx_queue_count, ops->rx_descriptor_status);
         printf ("  tx_pkt_burst=%p tx_pkt_prepare=%p tx_descriptor_status=%p\n",
                 ops->tx_pkt_burst, ops->tx_pkt_prepare, ops->tx_descriptor_status);
+    }
+
+    /* Stop and Close all the ports */
+    RTE_ETH_FOREACH_DEV (port_id)
+    {
+        rc = rte_eth_dev_stop (port_id);
+        if (rc == 0)
+        {
+            rc = rte_eth_dev_close (port_id);
+            if (rc != 0)
+            {
+                printf ("rte_eth_dev_close() failed : %s\n", strerror (-rc));
+            }
+        }
+        else
+        {
+            printf ("rte_eth_dev_stop() failed : %s\n", strerror (-rc));
+        }
     }
 
     /* clean up the EAL */
